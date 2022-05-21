746c1eeae9921bd997e2818daf4a456dbe647e22
yousifsamir <yousifysm@gmail.com>
2022-05-21 23:54:11 +0200

    Draw Operation/Target

--- ken_gui.py
+++ ken_gui.py
@@ -4,8 +4,8 @@ import sys
 
 BLACK = (220, 224, 223)
 WHITE = (255, 255, 255)
-WINDOW_HEIGHT = 800
-WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
+WINDOW_WIDTH = 600
 
 
 def checkborders(point, points):
@@ -21,6 +21,8 @@ blockSize = WINDOW_WIDTH//9
 
 
 def drawGrid(n):
+    
+
     blockSize = WINDOW_WIDTH//n
     # Set the size of the grid block
     for x in range(n):
@@ -34,12 +36,18 @@ def drawGrid(n):
             # points = sorted(list(((2,1),(1,1),(3,1),(2,2))))
             # for point in points:
             # checkborders(point,points)
-bi = (125, 210, 110)
+bi = (0,0,0)
 
+def draw_cages(n,cages):
+    for cage in cages:
+        draw_cage(n,cage[0],cage[1],cage[2])
+def draw_cage(n,_points,op,target):
 
-def draw_cages(_points,n):
     blockSize = WINDOW_WIDTH//n
     points = sorted(list(_points))
+    # target = 6
+    # op = '+'
+    # Draw Cages Borders:
     for point in points:
         x = checkborders(point, points)
         if x[0]:  # left (1,1) -> x = 0 *20 = 0 , y = 20  (0, 20)
@@ -55,17 +63,27 @@ def draw_cages(_points,n):
         if x[3]:  # t
             pygame.draw.line(SCREEN, bi, ((
                 point[1]-1)*blockSize, (point[0])*blockSize), ((point[1])*blockSize, (point[0])*blockSize), 3)
+    
+    
+    #draw Cage Operation and target
+    x,y=(points[0][1]-0.95)*blockSize,(points[0][0]-0.99)*blockSize
+    my_font = pygame.font.SysFont('Comic Sans MS', 20) ##
+    text_surface = my_font.render(f"{op}{target}", False, (0, 0, 0)) ##
+    SCREEN.blit(text_surface, (x,y)) ##
+    
 
 
 def main():
     global SCREEN, CLOCK
     pygame.init()
+    pygame.font.init() ##
     SCREEN = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
     CLOCK = pygame.time.Clock()
     SCREEN.fill(WHITE)
-    n=7
+    n=5
     drawGrid(n)
-    draw_cages(((1, 1), (1, 2), (2, 2), (2, 3)),n)
+    testcase = [(((1, 1), (1, 2), (2, 2), (2, 3)), '*', 8), (((2, 1), (3, 1)), '/', 5), (((4, 1), (4, 2), (5, 2), (5, 3)), '+', 15), (((5, 1),), '.', 3), (((3, 2),), '.', 3), (((1, 3), (1, 4)), '*', 15), (((3, 3),), '.', 2), (((4, 3), (4, 4), (3, 4), (3, 5)), '+', 13), (((2, 4),), '.', 4), (((5, 4),), '.', 2), (((1, 5), (2, 5)), '+', 5), (((4, 5), (5, 5)), '/', 5)]
+    draw_cages(n,testcase)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
