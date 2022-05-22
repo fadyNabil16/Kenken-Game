7afdb918b75fade289647e37700fa2c8c9299564
aminatef <48862934+aminatef@users.noreply.github.com>
2022-05-22 16:21:14 +0200

    Update csp.py

--- csp.py
+++ csp.py
@@ -209,3 +209,27 @@ def forward_checking(csp, var, value, assignment, removals):
 def mac(csp, var, value, assignment, removals):
     """Maintain arc consistency."""
     return AC3(csp, [(X, var) for X in csp.neighbors[var]], removals)
+def backtracking_search(csp,
+                        select_unassigned_variable=mrv,
+                        order_domain_values=unordered_domain_values,
+                        inference=no_inference):
+
+    def backtrack(assignment):
+        if len(assignment) == len(csp.variables):
+            return assignment
+        var = select_unassigned_variable(assignment, csp)
+        for value in order_domain_values(var, assignment, csp):
+            if 0 == csp.nconflicts(var, value, assignment):
+                csp.assign(var, value, assignment)
+                removals = csp.suppose(var, value)
+                if inference(csp, var, value, assignment, removals):
+                    result = backtrack(assignment)
+                    if result is not None:
+                        return result
+                csp.restore(removals)
+        csp.unassign(var, assignment)
+        return None
+
+    result = backtrack({})
+    assert result is None or csp.goal_test(result)
+    return result
